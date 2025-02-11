494ccc8726d8e93f1f7987520085bcd8ccac54d9
kaif969 <kaifsk969@gmail.com>
2025-02-12 01:42:55 +0530

    update img

--- src/GIPHome/ImageGrid.tsx
+++ src/GIPHome/ImageGrid.tsx
@@ -1,4 +1,6 @@
-
+import ProgramDuration from "../assets/Program Duration .png";
+import WhoShouldApply from "../assets/Who Should Apply.png";
+import HowToApply from "../assets/How to Apply .png";
 
 
 
@@ -6,17 +8,17 @@ const allData = [
     {
         subtitle: "Program Duration",
         description: "The Global Internship Program (GIP) is a remote, flexible internship lasting 3 to 6 months, allowing you to balance academic commitments while gaining career-ready skills. You’ll work on impactful projects, collaborate with global teams, and build a strong portfolio to stand out in today’s competitive job market.",
-        image: "",
+        image: ProgramDuration,
     },
     {
         subtitle: "Who Should Apply?",
         description: "We are looking for motivated, passionate students eager to learn and grow. If you're studying business, technology, data science, or other emerging fields, the GIP at Implica Global Co. is the perfect opportunity to gain hands-on experience while advancing your career.",
-        image: "",
+        image: WhoShouldApply,
     },
     {
         subtitle: "How to Apply",
         description: "Are you ready to take the next step in your career and gain meaningful experience? Apply for the Global Internship Program (GIP) today!",
-        image: "",
+        image: HowToApply,
     },
 ];
 
